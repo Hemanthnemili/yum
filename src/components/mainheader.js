0291cddf926b7afd18bf92bfe873156862ff53b4
Hemanthnemili <hemanth.nemili@gmail.com>
2024-01-28 18:30:55 +0530

    pages are added

---
+++ src/components/mainheader.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import Image from "next/image";
+
+export default function Mainheader() {
+  return (
+    <header className="text-white flex items-center justify-between p-5">
+      <Link href={"/"} className="flex gap-2 items-center">
+        <Image
+          src={logoImg}
+          width={70}
+          height={70}
+          alt="Picture of the author"
+          priority
+        />
+        Yum Foods
+      </Link>
+
+      <nav>
+        <ul className="flex gap-4 p-3 font-semibold">
+          <li>
+            <Link className=" text-white hover:text-green-600" href={"/meals"}>
+              Meals
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              className=" text-white hover:text-green-600"
+              href={"/community"}
+            >
+              Community
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
